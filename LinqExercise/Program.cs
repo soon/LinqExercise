88b4ebccb7357b42f2b759c332b475e20148ca80
Andrew Kuchev <0coming.soon@gmail.com>
2015-02-21 07:48:23 +0500

    Added Select tests

---
+++ LinqExercise/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqExercise
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static IEnumerable<int> AddTwoToEachElement(IEnumerable<int> enumerable)
+        {
+            return Enumerable.Empty<int>();
+        }
+
+        public static IEnumerable<int> NegatePositiveElements(IEnumerable<int> enumerable)
+        {
+            return Enumerable.Empty<int>();
+        }
+    }
+}
---
+++ Tests/TestSelect.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using LinqExercise;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class TestSelect
+    {
+        [TestMethod]
+        public void TestAddTwoToEachElement_EmptySequence()
+        {
+            CollectionAssert.AreEqual(new int[] {}, Program.AddTwoToEachElement(new int[] {}).ToList());
+        }
+
+        [TestMethod]
+        public void TestAddTwoToEachElement_OneElementInSequence()
+        {
+            CollectionAssert.AreEqual(new[] {1}, Program.AddTwoToEachElement(new [] {-1}).ToList());
+        }
+
+        [TestMethod]
+        public void TestAddTwoToEachElement_FiveElementsInSequence()
+        {
+            CollectionAssert.AreEqual(new[] {-2, 342, 23, 12, 3},
+                                      Program.AddTwoToEachElement(new[] {-4, 340, 21, 10, 1}).ToList());
+        }
+
+        [TestMethod]
+        public void TestNegatePositiveElements_EmptySequence()
+        {
+            CollectionAssert.AreEqual(new int[] {}, Program.NegatePositiveElements(new int[] {}).ToList());
+        }
+
+        [TestMethod]
+        public void TestNegatePositiveElements_OneElementInSequence()
+        {
+            CollectionAssert.AreEqual(new [] {-42}, Program.NegatePositiveElements(new [] {-42}).ToList());
+        }
+
+        [TestMethod]
+        public void TestNegatePositiveElements_FiveElementsInSequence()
+        {
+            CollectionAssert.AreEqual(new[] {-2, -1, 0, -1, -2},
+                                      Program.NegatePositiveElements(new[] {2, 1, 0, -1, -2}).ToList());
+        }
+    }
+}
